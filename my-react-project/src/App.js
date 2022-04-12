38e942225de30bd1135fe6df607ab6a5c7311f4a
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-04-12 21:30:10 +0700

    add components folder

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Main from './components/Main';
 
 export class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <Main />
+      </div>
     )
   }
 }
