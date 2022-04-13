17ba0407212c5ae19b7343891917a9c338f38734
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-04-13 18:44:29 +0700

    Update Components

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react'
-import Main from './components/Main';
+import React from 'react';
+import { renderRoutes } from 'react-router-config';
+import PropTypes from 'prop-types';
+import ErrorBoundary from './components/ErrorBoundry';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Main />
+const App = ({ route }) => {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <ErrorBoundary>{renderRoutes(route.routes)}</ErrorBoundary>
       </div>
-    )
-  }
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+App.propTypes = {
+  route: PropTypes.objectOf(PropTypes.any)
+};
+
+App.defaultProps = {
+  route: null
+};
+
+export default {
+  component: App
+};
--- my-react-project/src/components/Main.js
+++ my-react-project/src/components/Main.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
  
 class Main extends Component {
   render() {
